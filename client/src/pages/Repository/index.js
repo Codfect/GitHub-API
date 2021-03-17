36405a313564a94978a26fb609c29f0e74885a26
Oliver <oliverjgodoy@gmail.com>
2021-03-17 17:33:37 -0300

    static for data api

--- client/src/pages/Repository/index.js
+++ client/src/pages/Repository/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouteMatch, Link } from 'react-router-dom';
 import { FiCornerUpLeft } from 'react-icons/fi';
+import api from '../../services/api'
 
 import './styles.css'
 
 function Repository() {
+  const [repository, setRepository] = useState(null)
+
   const { params } = useRouteMatch();
 
+  useEffect(() => {
+    api.get(`users/${params.repository}`).then((response) => {
+      console.log(response.data);
+    })
+  }, [params.repository]);
+
   return(
-    <Link className="iconBack" to="/"> 
-      <FiCornerUpLeft size={18} />
-      Back
-    </Link>
+    <>
+      <Link className="iconBack" to="/"> 
+      <FiCornerUpLeft size={29} />
+      </Link>
+
+      <div className=" "> 
+        <div>
+          <strong>Codfect</strong>
+          <p>Oliver</p>
+        </div>
+      </div>
+    </>
   );
 }
 
