533791b274c07a206bc0d720815fcf443c5bdf72
Oliver <oliverjgodoy@gmail.com>
2021-03-17 01:03:21 -0300

    test listing git hub user

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -1,8 +1,33 @@
 import React from 'react';
 
+import './styles.css';
+
 function Dashboard() {
   return (
-    <h1>Dashboard</h1>
+    <>
+      <h1> Search repositories on Github </h1>
+
+      <form className="searchArea">
+        <input placeholder="Digite o nome do usuario" />
+
+        <button type="submit">Pesquisar</button>
+      </form>
+
+      <div className="repositories"> 
+        <a href="check">
+          <img 
+            src="https://avatars.githubusercontent.com/u/54602909?s=400&u=869175d1dcaa9abf9f6952279f7b3809cbcd26f8&v=4"
+            alt="Oliver teste"
+          />
+        
+          <div>
+              <strong>Codfect</strong>
+              <p>posso trazer a bio</p>
+          </div>
+
+        </a>
+      </div>
+    </>
   );
 }
 
--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+// Rotas importadas
 import Dashboard from '../pages/Dashboard';
 import Repository from '../pages/Repository';
 
+// Componentes com as rotas
 function Routes() {
   return(
     <BrowserRouter>
